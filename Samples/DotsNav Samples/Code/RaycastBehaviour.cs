5725009404e94adb17820a067b0ea432ec1ea5e3
Bas <bas@bassmit.info>
2021-05-24 11:59:26 +0200

    - remove samples, they break ci and are more conveniently viewed in the full project - fix demo menu in editor

--- Samples/DotsNav Samples/Code/RaycastBehaviour.cs
+++
@@ -1,9 +0,0 @@
-using Unity.Mathematics;
-using UnityEngine;
-
-public class RaycastBehaviour : MonoBehaviour
-{
-    public Transform Goal;
-    public float2 GetStart() => transform.position.xz();
-    public float2 GetGoal() => Goal.transform.position.xz();
-}
