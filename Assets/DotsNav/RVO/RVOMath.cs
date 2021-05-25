968ea339f1e0d6eeb69ebed76376b51b7a035e31
Bas <bas@bassmit.info>
2021-05-25 14:55:00 +0200

    use math.determinant

--- Assets/DotsNav/RVO/RVOMath.cs
+++
@@ -1,8 +0,0 @@
-﻿using Unity.Mathematics;
-
-static class RVOMath
-{
-
-    internal static float Determinant(float2 vector1, float2 vector2)
-        => vector1.x * vector2.y - vector1.y * vector2.x;
-}
