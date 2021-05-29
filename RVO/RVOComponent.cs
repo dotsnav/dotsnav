f43aee0f6db4892a47220084af2879ddac602941
Bas <bas@bassmit.info>
2021-05-29 14:00:10 +0200

    remove Position from RVOComponent

--- RVO/RVOComponent.cs
+++ RVO/RVOComponent.cs
@@ -3,7 +3,6 @@ using Unity.Mathematics;
 
 public struct RVOComponent : IComponentData
 {
-    public float2 Position;
     public float2 PrefVelocity;
     public float PrefSpeed;
     public float NeighbourDist;
