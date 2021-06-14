a3f3d02d732ae1303357fdc3d383b1e81c2a6b4d
Bas <bas@bassmit.info>
2021-06-14 11:05:53 +0200

    destroy bulk insertion entities

---
+++ Assets/DotsNav/Navmesh/Systems/DestroyBulkInsertionEntitiesSystem.cs
@@ -0,0 +1,27 @@
+﻿using DotsNav.Data;
+using DotsNav.Systems;
+using Unity.Entities;
+
+namespace DotsNav.Navmesh.Systems
+{
+    [UpdateInGroup(typeof(DotsNavSystemGroup))]
+    class DestroyBulkInsertionEntitiesSystem : SystemBase
+    {
+        protected override void OnUpdate()
+        {
+            var ecbSource = DotsNavSystemGroup.EcbSource;
+            var buffer = ecbSource.CreateCommandBuffer().AsParallelWriter();
+
+            Entities
+                .WithBurst()
+                .WithAny<VertexAmountElement, ObstacleBlobComponent>()
+                .ForEach((Entity entity, int entityInQueryIndex) =>
+                {
+                    buffer.DestroyEntity(entityInQueryIndex, entity);
+                })
+                .ScheduleParallel();
+
+            ecbSource.AddJobHandleForProducer(Dependency);
+        }
+    }
+}
