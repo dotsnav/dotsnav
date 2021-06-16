6cef2581bd32fe0e25ffe8d60c1da6ff68150d5c
Bas <bas@bassmit.info>
2021-06-16 11:54:41 +0200

    expose query on treecomponent

--- Assets/DotsNav/Core/Data/DynamicTreeComponent.cs
+++ Assets/DotsNav/Core/Data/DynamicTreeComponent.cs
@@ -6,5 +6,10 @@ namespace DotsNav.Data
     public struct DynamicTreeComponent : IComponentData
     {
         internal DynamicTree<Entity> Tree;
+
+        public void Query<T>(T collector, AABB aabb) where T : IQueryResultCollector<Entity>
+        {
+            Tree.Query(collector, aabb);
+        }
     }
 }
