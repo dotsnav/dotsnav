be404d469654ce40c57afc0055db433b65a55582
Bas <bas@bassmit.info>
2021-06-16 11:05:22 +0200

    move file

--- PathFinding/Data/PathQueryComponent.cs
+++ PathFinding/Data/PathQueryComponent.cs
@@ -1,7 +1,7 @@
 using Unity.Entities;
 using Unity.Mathematics;
 
-namespace DotsNav.PathFinding
+namespace DotsNav.PathFinding.Data
 {
     /// <summary>
     /// Entities with Agent DynamicBuffer&lt;PathSegmentElement&gt; and DynamicBuffer&lt;TriangleElement&gt; are considered for
